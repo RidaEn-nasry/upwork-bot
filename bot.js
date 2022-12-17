f6e393d34e0802f09352e813f58454927a4921ad
wa5ina <ren-nasr@student.1337.ma>
2022-12-17 15:47:50 +0100

    done...

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 
 // if there's a timeout error, wait for 10 seconds and run the script again 
 
+console.log('starting the script');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 require('dotenv').config();
@@ -113,26 +114,29 @@ function tooCheap(typeOfJob) {
 
 
 (async () => {
+    console.log('entering the async function');
+    // reading keywords from keywords.txt file
+    let keywords = fs.readFileSync('/Users/wa5ina/Porn/automation/upwork-bot/keywords.txt', 'utf-8');
+    keywords = keywords.split('\n');
+    for (let i = 0; i < keywords.length; i++) {
+        keywords[i] = keywords[i].trim();
+    }
+    // console.log(keywords);
+    // return;
     // Launch the browser in non-headless mode
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-
     // Set a realistic user agent string
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36');
 
-    // Navigate to the job search results page
+    // set the viewport to 1920x1080 to avoid the cookie banner 
     await page.setViewport({
         width: 1920,
         height: 1080
     });
     await page.goto('https://www.upwork.com/ab/account-security/login');
-
     // Wait for the page to load 
     await page.waitForTimeout(1000);
-
-    // set the high and width of the viewport
-
-    // get email and password from the .env file
 
     // getting the email and password from the .env file
     const email = process.env.EMAIL;
@@ -150,104 +154,78 @@ function tooCheap(typeOfJob) {
     // password
     await page.type('#login_password', password);
     await page.click('#login_control_continue');
-
     // move the mouse randomly to be more human 🤡
     for (let i = 0; i < 10; i++) {
         await page.mouse.move(getRndm(0, 20000), getRndm(0, 10000));
         await page.waitForTimeout(1000);
     }
-
     // wait for the page to load
     // wait for the search input to load 
-    await page.waitForSelector('input[placeholder="Search for job"]', { visible: true });
-    // then search for javascript jobs , in the input with placeholder 'Search for jobs'
-    await page.type('input[placeholder="Search for job"]', 'web development');
-    // click the search button
-    await page.click('button[data-test="job-search-button"]');
+    let allJobs = [];
 
+    // wait for search input to load
+    // await page.waitForSelector('input[placeholder="Search for job"]', { visible: true });
+    for (let i = 0; i < keywords.length; i++) {
+        console.log('searching for ' + keywords[i]);
+        await page.goto('https://www.upwork.com/ab/jobs/search/?q=' + keywords[i]);
+        if (i == 0) {
+            await page.waitForTimeout(3000);
+            await page.waitForSelector('div[data-test="jobs_per_page"]', { visible: true });
+            await page.click('div[data-test="jobs_per_page"]');
+            // wait for the dropdown menu to load
+            await page.waitForTimeout(5000);
+            // click in the <span> 50 </span>
+            await page.click('#dropdown-menu-6 li:nth-child(3) span');
+        }
 
-    // wait for the elemnt div[data-test="jobs_per_page"] to load
-    await page.waitForSelector('div[data-test="jobs_per_page"]', { visible: true });
-    // click the dropdown menu
-    await page.click('div[data-test="jobs_per_page"]');
-    // wait for the dropdown menu to load
-    await page.waitForTimeout(3000);
-    // select the 50 jobs per page , which is in <li> <span> 50 </span> </li>
-    // select the 50 jobs per page
-    await page.click('div[data-test="jobs_per_page"] ul li:nth-child(3)');
-    // wait for the page to load
-    // await page.waitForTimeout(3000);
+        // wait for the page to load
+        await page.waitForTimeout(3000);
+        await page.waitForSelector('div[data-test="main-tabs-index"]', { visible: true });
+        // get all sections with data-test="JobTile"
+        const listings = await page.$$('section[data-test="JobTile"]');
+        // change the page number of jobs
+        let jobs = await Promise.all(listings.map(async (listing) => {
+            // get the title of the job which in <h4 class="job-tile-title"> <a> </a> </h4>
+            let posted = await getTime(listing);
+            // if it's too old, then skip it
+            if (tooOld(posted) === true)
+                return;
+            let title = await getTitle(listing);
+            // run titles only for once
+            // get the link of the job which is in <h4 class="job-tile-title"> <a href="link"> </a> </h4>
+            let link = await getLink(listing);
+            // get the description of the job which in <span data-test="job-tile-description">
+            // description = await listing.$eval('span[data-test="job-description-text"]', (span) => span.innerText);
+            let description = await getDescription(listing);
+            // get type of job {type, budget}
+            let typeOfJob = await getTypeOfJob(listing);
+            if (tooCheap(typeOfJob) === true)
+                return;
+            // // is client's payment verified (true or false)
+            let paymentverified = await isVerified(listing);
+            return { posted, title, link, description, typeOfJob, paymentverified };
+        }
+        ));
+        // filter out the undefined jobs
+        jobs = jobs.filter((job) => job !== undefined);
+        // push jobs to alljobs
+        allJobs.push(...jobs);
 
-
-    // select the span
-
-
-    // select the 50 jobs per page
-
-    // then select the span
-    // select 50 jobs per page
-    // await page.click('div[data-test="jobs_per_page"] ul li:nth-child(3)');
-    // wait for the page to load
-    await page.waitForTimeout(3000);
-    await page.waitForSelector('div[data-test="main-tabs-index"]', { visible: true });
-
-    // get all sections with data-test="JobTile"
-    // we gonna go through three pagination pages
-    // let jobs = [];
-
-    const listings = await page.$$('section[data-test="JobTile"]');
-
-    // console.log(listings.length);
-
-
-    // change the page number of jobs
-    let jobs = await Promise.all(listings.map(async (listing) => {
-        // get the title of the job which in <h4 class="job-tile-title"> <a> </a> </h4>
-        // title = await listing.$eval('h4.job-tile-title a', (a) => a.innerText);
-        // console.log("I'm the listing\n***********************************");
-        // console.log(listing.toString());
-        // console.log("***********************************");
-        let posted = await getTime(listing);
-
-        // if it's too old, then skip it
-        if (tooOld(posted) === true)
-            return;
-
-        let title = await getTitle(listing);
-        // run titles only for once
-
-        // get the link of the job which is in <h4 class="job-tile-title"> <a href="link"> </a> </h4>
-        // link = await listing.$eval('h4.job-tile-title a', (a) => a.href);
-        let link = await getLink(listing);
-
-        // get the description of the job which in <span data-test="job-tile-description">
-        // description = await listing.$eval('span[data-test="job-description-text"]', (span) => span.innerText);
-        let description = await getDescription(listing);
-        // get type of job {type, budget}
-        // typeOfJob = { type: '', budget: '' };
-        let typeOfJob = await getTypeOfJob(listing);
-
-        if (tooCheap(typeOfJob) === true)
-            return;
-        // typeOfJob = "";
-        // // is client's payment verified (true or false)
-        // paymentverified = false;
-        let paymentverified = await isVerified(listing);
-
-        return { posted, title, link, description, typeOfJob, paymentverified };
     }
-    ));
-
 
     // filter out the undefined jobs
-    jobs = jobs.filter((job) => job !== undefined);
-    console.log(jobs);
+    // save the jobs to a json file
+
     // Add some randomness to the requests
     const randomDelay = Math.random() * 2000;
     await page.waitForTimeout(randomDelay);
 
     // Close the browser
     await browser.close();
+    // write to json file by overriding the file
+    console.log('Writing to json file');
+    fs.writeFileSync('/Users/wa5ina/Porn/automation/upwork-bot/jobs.json', JSON.stringify(allJobs, null, 2));
+    console.log('Done');
 })();
 
 
---
+++ notifyjobs.sh
@@ -0,0 +1,56 @@
+#!/bin/zsh
+# this script will read the jobs.json file and send a notification the user 
+# for each job in the file
+file=$(cat jobs.json)
+# using sed to remove the first and last line of the json file
+jobs=$(echo $file | /opt/homebrew/bin/jq '.[]')
+title_tmp=$(echo $jobs | /opt/homebrew/bin/jq '.title')
+posted_tmp=$(echo $jobs | /opt/homebrew/bin/jq '.posted')
+type_tmp=$(echo $jobs | /opt/homebrew/bin/jq '.typeOfJob.type')
+budget_tmp=$(echo $jobs | /opt/homebrew/bin/jq '.typeOfJob.budget')
+link_tmp=$(echo $jobs | /opt/homebrew/bin/jq '.link')
+
+# store the titles in an array
+titles=($(echo "$title_tmp" | sed "s/ /-/g"))
+posted=($(echo "$posted_tmp" | sed "s/ /-/g"))
+types=($(echo "$type_tmp" | sed "s/ /-/g"))
+budgets=($(echo "$budget_tmp" | sed "s/ /-/g"))
+links=($(echo "$link_tmp" | sed "s/ /-/g"))
+
+# logo=""
+brave="/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
+# get length of an array
+titlesLength=${#titles[@]}
+
+for (( i=0; i<${titlesLength}; i++ ));
+do 
+    # getting each field at a time
+    #removing the "-" and "\"" from the title
+    title=${titles[$i]}
+    title=$(echo $title | sed "s/-/ /g" | sed "s/\"//g")
+    posted_each=${posted[$i]}
+    posted_each=$(echo $posted_each | sed "s/-/ /g" | sed "s/\"//g")
+    type_each=${types[$i]}
+    type_each=$(echo $type_each | sed "s/-/ /g" | sed "s/\"//g")
+    budget_each=${budgets[$i]}
+    budget_each=$(echo $budget_each | sed "s/-/ /g" | sed "s/\"//g")
+    link_each=${links[$i]}
+    link_each=$(echo $link_each | sed "s/-/ /g" | sed "s/\"//g")
+    if [ $i -eq 0 ]; then
+        continue
+    fi
+    answer=$(/Users/wa5ina/bin/alerter.sh  -title "$title" -subtitle "posted: $posted_each " -message "type: $type_each budget: $budget_each" -timeout 10 -actions "open" -sender "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser")
+    if [ "$answer" = "open" ]; then
+        open -a   "$brave" "$link_each"
+    fi
+    sleep 3
+
+done
+echo "" > jobs.json
+
+
+
+
+    
+
+
